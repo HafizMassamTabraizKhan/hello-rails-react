4fba13ed79f3908f3ed932ae44c6160f9f8b959e
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-11-07 16:30:21 +0500

    Add greeting model, controller and redux

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store.js';
 import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
 
-function App() {
-  return (<h1>Hello World!</h1>);
-}
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
